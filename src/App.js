7ebc4e3e4a0dd632679e3f6b8cd5650687f8c42c
Cymbelmine <74141947+Cymbelmine@users.noreply.github.com>
2021-02-14 15:20:25 -0600

    Deleted a header element in App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
        <h1>Weather App</h1>
-      </header>
       <Weather city="Berlin"/>
       <WeatherFramework />
     </div>
