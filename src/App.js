66b2406981e17c3e943a653693db38c588f46487
Cymbelmine <74141947+Cymbelmine@users.noreply.github.com>
2021-02-14 09:30:51 -0600

    Update App.Css
    Create css-framework for weather-app

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Weather App!</h1>
-       <Weather city="Berlin"/>
-  
+       <h1>Weather App</h1>
       </header>
+      <Weather city="Berlin"/>
     </div>
   );
 }
