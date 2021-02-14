4b9857e89d0de7498207cb374bda182e20562a2b
Cymbelmine <74141947+Cymbelmine@users.noreply.github.com>
2021-02-14 15:58:12 -0600

    Added links to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ export default function App() {
   return (
     <div className="App">
        <h1>Weather App</h1>
+       <footer>
+         This project was created by <a href="https://stepbystep.hashnode.dev/" target="_blank">Mel Wieschalla</a> and is 
+         <a href="https://github.com/Cymbelmine/weather-react" target="_blank"> open-sourced on GitHub</a>
+       </footer>
       <Weather city="Berlin"/>
       <WeatherFramework />
     </div>
