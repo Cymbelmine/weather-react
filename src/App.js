bb15f38fa2dbac8703607eaa3de02481417953d4
Cymbelmine <74141947+Cymbelmine@users.noreply.github.com>
2021-02-21 09:55:03 -0600

    Added bootstrap to Index.js
    Added a container to App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-       <h1>Weather App</h1>
+      <h1>Weather App</h1>
+      <div className="container">
        <footer>
          This project was created by <a href="https://stepbystep.hashnode.dev/" target="_blank">Mel Wieschalla</a> and is 
          <a href="https://github.com/Cymbelmine/weather-react" target="_blank"> open-sourced on GitHub</a>
        </footer>
       <Weather city="Berlin"/>
       <WeatherFramework />
+    </div>
     </div>
   );
 }
