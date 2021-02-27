91a1224fd35490d7709aed36ca20c4b2917c0af8
Cymbelmine <74141947+Cymbelmine@users.noreply.github.com>
2021-02-27 13:04:51 -0600

    Deleted prototype
    Deleted WeatherFramework.js
    Deleted WeahterFramework.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Weather from "./Weather";
-import WeatherFramework from "./WeatherFramework";
 import './App.css';
 
 export default function App() {
@@ -12,7 +11,6 @@ export default function App() {
          This project was created by <a href="https://stepbystep.hashnode.dev/" target="_blank">Mel Wieschalla</a> and is 
          <a href="https://github.com/Cymbelmine/weather-react" target="_blank"> open-sourced on GitHub</a>
        </footer>
-      <WeatherFramework />
     </div>
     </div>
   );
